6922a72a3ecd48540223891bdeefb16c3e6a999e
Ivan <ivannantes2004@gmail.com>
2024-08-09 22:08:47 -0300

    Regex CEP - não funcional

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+const cepRegex = /^[0-9]{5}-?[09]{3}$/;
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
@@ -18,7 +20,9 @@ app.get('/consulta-cep/:cep', async (req, res) => {
     try {
         const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
         res.json(response.data);
+        
     } catch (error) {
+        cepRegex.test(cep);
         console.error('Erro ao fazer requisição:', error);
         res.status(500).send('Erro ao consultar o CEP');
         
